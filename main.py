bf9ca50efc300fb188bed91c2c842e7b9747e53c
032-shi <0324omitsu@gmail.com>
2021-08-07 20:25:52 +0900

    [add]音声ファイル形式の注意コメント追記

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+# coding: UTF-8
 import speech_recognition as sr
 
 r = sr.Recognizer()
 
-with sr.AudioFile("sample.wav") as source:
+with sr.AudioFile("sample.wav") as source: #ここに音声ファイルを指定する（wav形式のファイルを使用すること）
   audio = r.record(source)
 
 text = r.recognize_google(audio, language='ja-JP')
